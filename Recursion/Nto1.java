86e809cf22ba1bb69b899896b5f49653f4f66745
MAHIMAMISHRA0203 <mishramahima2004@gmail.com>
2026-06-03 13:49:19 +0530

    solved 3 problems

---
+++ Recursion/Nto1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void printNumbers(int n) {
+        // Your code goes here
+        if(n==0)return;
+        printNumbers(n-1);
+        System.out.println(n);
+    }
+}
